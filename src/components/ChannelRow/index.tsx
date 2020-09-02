12524bb90ea2cdbf82d4e4992aaf98177f881d1c
unknown <viniciusbeloti7@gmail.com>
2020-09-02 14:19:21 -0300

    feat: add ChannelRow

---
+++ src/components/ChannelRow/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const ChannelRow: React.FC = () => {
+  return <div />;
+}
+
+export default ChannelRow;
--- src/components/SearchPage/index.tsx
+++ src/components/SearchPage/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined'
 
 import './styles.css';
+import ChannelRow from '../ChannelRow';
 
 const SearchPage: React.FC = () => {
   return (
     <div className="searchPage">
-
+      <div className="searchPage__filter">
+        <TuneOutlinedIcon />
+        <h2>FILTRO</h2>
+      </div>
+      <hr />
+      <ChannelRow />
     </div>
   );
 }
